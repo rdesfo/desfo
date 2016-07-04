ad26af84a292ecb4d86b3a9072af3ebd0d77af6e
Ryan Desfosses <ryan@desfo.org>
2016-07-04 00:43:34 -0400

    added git ignore and shake file

--- Shake.hs
+++ Shake.hs
@@ -1,12 +1,21 @@
 import Development.Shake
 
-
 -- shakeFiles testes shake to make .shake in dist-newstyle
 main = shakeArgs shakeOptions{shakeFiles="dist-newstyle"} $ do
     want ["push"]
 
     phony "push" $ do
+      () <- cmd "git stash save"
       () <- cmd "git checkout gh-pages"
-      () <- removeFiles "test" ["*"]
-      cmd "ls"
-
+      () <- liftIO $ removeFiles "." ["*.html"] -- remove index
+      () <- liftIO $ removeFiles "images" ["*.png"] -- remove images
+      () <- liftIO $ removeFiles "posts" ["*.html"] -- remove posts
+      () <- liftIO $ removeFiles "css" ["*.css"] -- remove css
+      () <- cmd "cp -ar _/site/. ."
+      () <- liftIO $ removeFiles "_cache" ["//*"]
+      () <- liftIO $ removeFiles "_site" ["//*"]
+      () <- cmd "git add ."
+      () <- cmd "git commit"
+      () <- cmd "git push gh-pages origin/gh-pages"
+      () <- cmd "git checkout master"
+      cmd "git stash pop"
