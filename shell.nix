c93b385a93f11f56896c7ff1cf06ca617be3e3f7
Ryan Desfosses <ryan@desfo.org>
2016-07-04 00:42:00 -0400

    added nix shell file

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, hakyll, shake, stdenv }:
+      mkDerivation {
+        pname = "desfo-org";
+        version = "0.1.0.1";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base hakyll shake ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
