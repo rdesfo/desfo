409eb10cf3835bee0077309972c58d9d535f2be4
Ryan Desfosses <ryan@desfo.org>
2016-08-30 00:26:33 -0400

    removed leftover demo posts and added post list to activity section

--- site.hs
+++ site.hs
@@ -15,26 +15,26 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
---    match "posts/*" $ do
---        route $ setExtension "html"
---        compile $ pandocCompiler
---            >>= loadAndApplyTemplate "templates/post.html"    postCtx
---            >>= loadAndApplyTemplate "templates/default.html" postCtx
---            >>= relativizeUrls
---
---    create ["archive.html"] $ do
---        route idRoute
---        compile $ do
---            posts <- recentFirst =<< loadAll "posts/*"
---            let archiveCtx =
---                    listField "posts" postCtx (return posts) `mappend`
---                    constField "title" "Archives"            `mappend`
---                    defaultContext
---
---            makeItem ""
---                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
---                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
---                >>= relativizeUrls
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
 
 
     match "index.html" $ do
