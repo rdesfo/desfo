4324aadd72e83301b807c3e9af413be356b831aa
Ryan Desfosses <ryan@desfo.org>
2017-03-31 16:57:41 +0000

    added keybase file

--- site.hs
+++ site.hs
@@ -11,6 +11,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "keybase.txt" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
